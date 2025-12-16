685b47a4af0fb81a4544fe639e34c3d047731100
tech daddi <127457972+techdaddi@users.noreply.github.com>
2025-12-16 09:11:05 -0600

    Enable React strict mode and SWC minification

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
